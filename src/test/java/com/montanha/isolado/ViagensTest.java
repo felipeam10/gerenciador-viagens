1628617fbb332bec80f73660b97a9cd25c79e9da
felipeam10 <felipeam10@hotmail.com>
2021-12-09 00:04:08 -0300

    v7.4 iniciando rest-assured no projeto

--- src/test/java/com/montanha/isolado/ViagensTest.java
+++ src/test/java/com/montanha/isolado/ViagensTest.java
@@ -1,17 +1,30 @@
 package com.montanha.isolado;
 
+import io.restassured.http.ContentType;
 import org.junit.Assert;
 import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class ViagensTest {
 
     @Test
     public void testCadastroDeViagemValidoRetornaSucesso() {
-        Assert.assertEquals(1,1);
+        // Configurações Rest-Assured
+        baseURI = "http://localhost";
+        port =  8089;
+        basePath = "/api";
+
+        given()
+            .contentType(ContentType.JSON)
+            .body("{\"email\": \"admin@email.com\",\"senha\": \"654321\"}")
+        .when()
+            .post("/v1/auth")
+        .then()
+            .log()
+                .all();
     }
 
-    @Test
-    public void testCadastroDeViagemInvalidoRetornaFalha() {
-        Assert.assertEquals(1, 2);
-    }
+
 }
