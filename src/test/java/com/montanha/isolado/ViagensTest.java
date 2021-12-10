e05097e704ac1d1804627313bbdf87e4a589e502
felipeam10 <felipeam10@hotmail.com>
2021-12-10 01:12:46 -0300

    v7.5 Extraindo o token, fazendo uma segunda Requisição e fazendo Asserções

--- src/test/java/com/montanha/isolado/ViagensTest.java
+++ src/test/java/com/montanha/isolado/ViagensTest.java
@@ -16,15 +16,34 @@ public class ViagensTest {
         port =  8089;
         basePath = "/api";
 
-        given()
+        String token = given()
             .contentType(ContentType.JSON)
             .body("{\"email\":\"admin@email.com\",\"senha\":\"654321\"}")
         .when()
             .post("/v1/auth")
         .then()
-            .log()
-                .all();
-    }
+            .extract()
+                .path("data.token");
 
+
+        given()
+            .contentType(ContentType.JSON)
+            .body("{\n" +
+                    "  \"acompanhante\": \"AcompanhanteTeste\",\n" +
+                    "  \"dataPartida\": \"2021-14-10\",\n" +
+                    "  \"dataRetorno\": \"2021-14-10\",\n" +
+                    "  \"localDeDestino\": \"Ceara\",\n" +
+                    "  \"regiao\": \"Norte\"\n" +
+                    "}")
+            .header("Authorization", token)
+        .when()
+                .post("/v1/viagens")
+        .then()
+            .assertThat()
+                .statusCode(201)
+                .body("data.localDeDestino", equalTo("Ceara"))
+                .body("data.acompanhante", equalToIgnoringCase("acompanhanteteste"));
+
+    }
 
 }
